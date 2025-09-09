194602085a8df7a0f38b38ae4c3f319a9abedcdd
Serge Castro <serge@emiramed.com>
2025-09-09 09:59:18 -0700

    Fix case sensitivity issue: rename App.jsx to app.jsx and update imports

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s0mckkpa0ro"
+    "revision": "0.eskieq2imt"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Should be capital 'A'
+import App from './app'; // Capital 'A', no extra dots or paths
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
