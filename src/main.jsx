2a440f55cd58546437e2ff8d22f3858e74ff3234
Serge Castro <serge@emiramed.com>
2025-09-09 08:38:41 -0700

    Fixed casing issue by updating App.jsx and cleaning backups

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';  // Import the main App component
+import App from './App'; // Should be capital 'A'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />  // Render the full CryptoPricer app
+    <App />
   </React.StrictMode>,
 );
